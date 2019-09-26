978655843ebfdf3eaf068fec808d0a6e4c32232c
Alouiscious Media <info@reneesoul.com>
2019-09-26 03:15:39 -0700

    git even

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,4 @@
-"SELECT species FROM"
+-- binding.pry
+-- "SELECT characters.species.id.last FROM characters WHERE character.species.id.last = "Martian";"
+UPDATE characters 
+SET species = "Martian" WHERE id = 8;
